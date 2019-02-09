4d6d939a5e36c6e4ca864aee865c013449120971
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-02-09 20:07:47 +0000

    fix server alias

--- fish/.config/fish/functions/server.fish
+++ fish/.config/fish/functions/server.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function server --description 'alias server python -m SimpleHTTPServer'
-	python -m SimpleHTTPServer $argv;
+function server --description 'alias server python2 -m SimpleHTTPServer'
+    python2 -m SimpleHTTPServer $argv;
 end
