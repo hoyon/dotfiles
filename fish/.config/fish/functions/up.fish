906fe303e0243f1c3caf4d1d549a5e361c871709
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-02-09 22:54:19 +0000

    make up script do more

--- fish/.config/fish/functions/up.fish
+++ fish/.config/fish/functions/up.fish
@@ -1,4 +1,13 @@
-# Defined in - @ line 1
-function up --description 'alias up yay -Syu --combinedupgrade'
-	yay -Syu --combinedupgrade $argv;
+function up --description 'bring system up to date'
+    set host (hostname)
+
+    if test "$host" = "hoyon-desktop"
+        yay -Syu --combinedupgrade $argv
+    else if test "$host" = "hoyon-thinkpad"
+        sudo apt-get update
+        sudo apt upgrade
+    end
+
+    git -C ~/.dotfiles pull
+    git -C ~/.emacs.d pull
 end
