3f4cc4f8aaabb8f127e59ed250f77b3841e35305
Ho-Yon Mak <ho-yon.mak@whitehat.org.uk>
2019-04-01 16:18:44 +0100

    doom emacs config tweaks

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -2,6 +2,8 @@
 
 ;; Place your private configuration here
 
+(setq doom-font (font-spec :family "Source Code Pro" :size 12))
+
 (global-visual-line-mode) ;; Always wrap long lines
 (setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow)) ;; Show arrows on wrapped lines
 
@@ -15,7 +17,8 @@
         (:prefix ("t" . "test")
           :desc "Run all tests"                   "a" #'alchemist-mix-test
           :desc "Run tests in this buffer"        "b" #'alchemist-mix-test-this-buffer
-          :desc "Run tests at point"              "t" #'alchemist-mix-test-at-point)
+          :desc "Run tests at point"              "t" #'alchemist-mix-test-at-point
+          :desc "Rerun last run tests"            "r" #'alchemist-mix-rerun-last-test)
         (:prefix ("g" . "goto")
           :desc "Toggle file and test"            "t" #'alchemist-project-toggle-file-and-tests)))
 
