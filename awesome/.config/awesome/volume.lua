dd8b4ce2d4c81f6cc873b867d0e42b65c527babc
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-11-04 21:27:40 +0000

    attempt at setting correct volume

--- awesome/.config/awesome/volume.lua
+++ awesome/.config/awesome/volume.lua
@@ -11,40 +11,55 @@ volume_widget:buttons(awful.util.table.join(
     awful.button({}, 2, function() awful.spawn("pavucontrol") end)
 ))
 
+sink_id = ""
+
+awful.spawn.easy_async("/home/hoyon/.config/awesome/pulse-sink.sh", function(stdout)
+    sink_id = " --id " .. stdout .. " "
+end)
+
+function status_command()
+    return "pulsemixer" .. sink_id .. " --get-volume --get-mute"
+end
+
 function volume_widget:update_volume()
-    awful.spawn.easy_async("amixer get Master", function(stdout)
-        local volume = string.match(stdout, "(%d?%d?%d)%%")
+    awful.spawn.easy_async(status_command(), function(stdout)
+        
+        local volume = string.match(stdout, "%d+")
         volume = tonumber(volume)
         volume = string.format("% 3d", volume)
 
-        local status = string.match(stdout, "%[(o[^%]]*)%]")
+        local status = tonumber(string.sub(stdout, -2))
 
         local volume_string = ""
-        if string.find(status, "on", 1, true) then
+        if status == 0 then
             volume_string = " " .. volume .. "%"
         else
-            volume_string = " " .. volume_string .. "Mute"
+            volume_string = " Mute"
         end
         self:set_markup(volume_string .. " | ")
     end)
 end
 
 function volume_widget:show_volnoti()
-    awful.spawn.easy_async("amixer get Master", function(stdout)
-        local volume = string.match(stdout, "(%d?%d?%d)%%")
+    awful.spawn.easy_async(status_command(), function(stdout)
+        local volume = string.match(stdout, "%d+")
         volume = tonumber(volume)
-        local volume_string = string.format("% 3d", volume)
+        volume = string.format("% 3d", volume)
+
+        local status = tonumber(string.sub(stdout, -2))
+
         -- check mute
-        if string.find(stdout, "off") then
-            awful.spawn("volnoti-show -m " .. volume_string, false)
+        if status == 0 then
+            awful.spawn("volnoti-show " .. volume, false)
         else
-            awful.spawn("volnoti-show " .. volume_string, false)
+            awful.spawn("volnoti-show -m " .. volume, false)
         end
     end)
 end
 
 function volume_widget:up(hide)
-    awful.spawn.easy_async("amixer set Master 5%+", function()
+    local command = "pulsemixer" .. sink_id .. " --change-volume +5"
+    awful.spawn.easy_async(command, function()
         self:update_volume()
         if not hide then
             self:show_volnoti()
@@ -53,7 +68,8 @@ function volume_widget:up(hide)
 end
 
 function volume_widget:down(hide)
-    awful.spawn.easy_async("amixer set Master 5%-", function()
+    local command = "pulsemixer" .. sink_id .. " --change-volume -5"
+    awful.spawn.easy_async(command, function()
         self:update_volume()
         if not hide then
             self:show_volnoti()
@@ -62,7 +78,8 @@ function volume_widget:down(hide)
 end
 
 function volume_widget:mute(hide)
-    awful.spawn.easy_async("amixer set Master toggle", function()
+    local command = "pulsemixer" .. sink_id .. " --toggle-mute"
+    awful.spawn.easy_async(command, function()
         self:update_volume()
         if not hide then
             self:show_volnoti()
