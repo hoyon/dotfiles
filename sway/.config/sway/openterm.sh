e3cb06d6232b50914d779f553f16c1671a2fdd88
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-12-29 18:23:51 +0000

    sway: fix bashisms

--- sway/.config/sway/openterm.sh
+++ sway/.config/sway/openterm.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 FOCUSED=$(swaymsg -t get_tree | jq '.. | (.nodes? // empty)[] | select(.focused==true) | {app_id, pid}')
 
 if [[ $FOCUSED =~ "termite" ]]; then
     PID=$(echo "$FOCUSED" | jq .pid)
-    CHILD_PID=$(pgrep -P $PID)
+    CHILD_PID=$(pgrep -P "$PID")
     CWD=$(readlink "/proc/$CHILD_PID/cwd")
     exec termite -d "$CWD"
 else
