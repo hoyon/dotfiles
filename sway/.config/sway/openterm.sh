2c3e7dc1d18a0eddfa378b722cb66f58fe6211af
Ho-Yon Mak <hoyon.mak@gmail.com>
2019-12-29 17:43:56 +0000

    sway: swayified openterm.sh

---
+++ sway/.config/sway/openterm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+FOCUSED=$(swaymsg -t get_tree | jq '.. | (.nodes? // empty)[] | select(.focused==true) | {app_id, pid}')
+
+if [[ $FOCUSED =~ "termite" ]]; then
+    PID=$(echo "$FOCUSED" | jq .pid)
+    CHILD_PID=$(pgrep -P $PID)
+    CWD=$(readlink "/proc/$CHILD_PID/cwd")
+    exec termite -d "$CWD"
+else
+    exec termite
+fi
