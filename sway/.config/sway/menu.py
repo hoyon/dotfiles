7cf76fde2ef383948ae8f570a37bacbf2341c21b
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-01-01 14:48:42 +0000

    sway: command menu

---
+++ sway/.config/sway/menu.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import subprocess
+
+
+def sway_cmd(cmd):
+    return lambda: subprocess.run(["swaymsg", cmd])
+
+
+commands = {
+    "Split horizontal": sway_cmd("split horizontal"),
+    "Split vertical": sway_cmd("split vertical"),
+}
+
+keys = list(commands.keys())
+
+options = "\n".join(keys)
+
+selection = subprocess.run(["wofi",
+                            "--dmenu",
+                            "--insensitive",
+                            "--cache-file", "/dev/null",
+                            "--prompt", "Enter a command..."],
+                           capture_output=True,
+                           input=options.encode())
+
+trimmed = selection.stdout.decode().strip()
+
+if trimmed == "":
+    exit()
+
+command = commands.get(trimmed)
+
+if command:
+    command()
+else:
+    message = f"Invalid command: {trimmed}"
+    subprocess.run(["notify-send", "-t", "2000", message])
