43bb59e529d96e4a3519a827e6e3a8e5c63e6bab
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-01-04 13:51:18 +0000

    use cut instead of awk

--- sway/.config/sway/switch-window.sh
+++ sway/.config/sway/switch-window.sh
@@ -5,6 +5,6 @@ WINDOWS=$(swaymsg -t get_tree | jq -r '.nodes | .[] | .nodes | . [] | select(.no
 TARGET=$(echo "$WINDOWS" | wofi --dmenu --cache-file /dev/null --prompt "Switch to window" --insensitive)
 
 if [ -n "$TARGET" ]; then
-    ID=$(echo "$TARGET" | awk '{print $1}')
+    ID=$(echo "$TARGET" | cut -d ' ' -f1)
     swaymsg [con_id="$ID"] focus
 fi
