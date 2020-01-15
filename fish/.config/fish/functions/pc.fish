9bc67155e8c6fefd87e8544dd94d677a7b915cb0
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-01-15 22:13:00 +0000

    fish: update pc (pkg-config) function

--- fish/.config/fish/functions/pc.fish
+++ fish/.config/fish/functions/pc.fish
@@ -1,4 +1,3 @@
-# Defined in /home/hoyon/.config/fish/config.fish @ line 16
 function pc --description 'fishy pkg-config'
-	command pkg-config $argv | string split " "
+	command pkg-config $argv | string split -n " "
 end
