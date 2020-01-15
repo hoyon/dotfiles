a37deb464b8afb1245d77b181d15d27cd077a16e
Ho-Yon Mak <ho-yon.mak@whitehat.org.uk>
2020-01-15 09:39:38 +0000

    doom: remove wl-copy snippet

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -113,21 +113,3 @@
 (put 'magit-todos-exclude-globs 'safe-local-variable #'listp)
 (put 'projectile-project-test-cmd 'safe-local-variable #'stringp)
 (put 'projectile-project-run-cmd 'safe-local-variable #'stringp)
-
-;; use wl-copy and wl-paste
-(setq wl-copy-process nil)
-(defun wl-copy (text)
-  (setq wl-copy-process (make-process :name "wl-copy"
-                                      :buffer nil
-                                      :command '("wl-copy" "-f" "-n")
-                                      :connection-type 'pipe))
-  (process-send-string wl-copy-process text)
-  (process-send-eof wl-copy-process))
-
-(defun wl-paste ()
-  (if (and wl-copy-process (process-live-p wl-copy-process))
-      nil ; should return nil if we're the current paste owner
-    (shell-command-to-string "wl-paste -n | tr -d \r")))
-
-(setq interprogram-cut-function 'wl-copy)
-(setq interprogram-paste-function 'wl-paste)
