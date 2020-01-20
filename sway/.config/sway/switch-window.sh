b38d1e8a06420e8c84bf8110ab47f80ae4294d89
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-01-20 19:29:54 +0000

    sway: fix cut

--- sway/.config/sway/switch-window.sh
+++ sway/.config/sway/switch-window.sh
@@ -5,6 +5,6 @@ WINDOWS=$(swaymsg -t get_tree | jq -re '.nodes | .[] | .nodes | . [] | select(.n
 TARGET=$(echo "$WINDOWS" | wofi --dmenu --cache-file /dev/null --prompt "Switch to window" --insensitive)
 
 if [ -n "$TARGET" ]; then
-    ID=$(echo "$TARGET" | cut -d ' ' -f1)
+    ID=$(echo "$TARGET" | cut -f1)
     swaymsg [con_id="$ID"] focus
 fi
