f7eb57d6b3350d9fce12be899916f4d9af5215b4
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-01-20 19:29:02 +0000

    sway: show app_id/class in window switcher

--- sway/.config/sway/switch-window.sh
+++ sway/.config/sway/switch-window.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-WINDOWS=$(swaymsg -t get_tree | jq -r '.nodes | .[] | .nodes | . [] | select(.nodes != null) | .nodes | .[] | select(.name != null) | "\(.id?) \(.name?)"')
+WINDOWS=$(swaymsg -t get_tree | jq -re '.nodes | .[] | .nodes | . [] | select(.nodes != null) | .nodes | .[] | select(.name != null) | [.id, .name, (.window_properties.class? | select(.!= null)), .app_id? | select(.!=null)] | "\(.[0])\t \(.[1]) - \(.[2])"')
 
 TARGET=$(echo "$WINDOWS" | wofi --dmenu --cache-file /dev/null --prompt "Switch to window" --insensitive)
 
