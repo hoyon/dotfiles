643955e861f568078066c10371407a0a621c41d5
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-02-22 21:49:53 +0000

    doom: no longer need to add eex to web mode alist

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -79,7 +79,6 @@
 
 ;; Web mode
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html\\.eex\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
 (setq web-mode-markup-indent-offset 2
       web-mode-css-indent-offset 2
