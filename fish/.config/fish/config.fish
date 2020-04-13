0633d7cd083a5bcfda04258914606922b0e80695
Ho-Yon Mak <hoyon.mak@gmail.com>
2020-04-13 18:37:47 +0100

    fish: improve less colours

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,13 @@
 set fish_greeting ""
 
 # Colourful man in less
-set -x LESS_TERMCAP_mb (printf "\033[01;31m")
-set -x LESS_TERMCAP_md (printf "\033[01;31m")
-set -x LESS_TERMCAP_me (printf "\033[0m")
-set -x LESS_TERMCAP_se (printf "\033[0m")
-set -x LESS_TERMCAP_so (printf "\033[01;44;33m")
-set -x LESS_TERMCAP_ue (printf "\033[0m")
-set -x LESS_TERMCAP_us (printf "\033[01;32m")
+set -x LESS_TERMCAP_mb (printf "\\e[1;91m")    # start blinking
+set -x LESS_TERMCAP_md (printf "\\e[1;91m")    # start bold
+set -x LESS_TERMCAP_me (printf "\\e[0m")       # end mode
+set -x LESS_TERMCAP_so (printf "\\e[1;40;93m") # start standout
+set -x LESS_TERMCAP_se (printf "\\e[0m")       # end standout
+set -x LESS_TERMCAP_us (printf "\\e[1;92m")    # start underlining
+set -x LESS_TERMCAP_ue (printf "\\e[0m")       # end underlining
 
 set SHELL /usr/bin/fish
 
