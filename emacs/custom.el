c742cef8a2228e7eba88963267a2171a8d8296d8
Ho-Yon Mak <hoyon.mak@gmail.com>
2021-05-22 22:59:48 +0100

    emacs: more config

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("0feb7052df6cfc1733c1087d3876c26c66410e5f1337b039be44cb406b6187c6" default)))
+   '("27a1dd6378f3782a593cc83e108a35c2b93e5ecc3bd9057313e1d88462701fcd" "0feb7052df6cfc1733c1087d3876c26c66410e5f1337b039be44cb406b6187c6" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
