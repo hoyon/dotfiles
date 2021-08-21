64f321b1c6e00f2541581a7eaa76c53d77fea435
Ho-Yon Mak <hoyon.mak@gmail.com>
2021-08-21 11:25:13 +0100

    waybar: ibus-engine takes japanese engine as arg

--- waybar/.config/waybar/ibus/ibus-engine.cpp
+++ waybar/.config/waybar/ibus/ibus-engine.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include <string>
 
-std::string japanese_json = R"({"text":"日本語"})";
+const std::string japanese_json = R"({"text":"日本語"})";
+std::string japanese_engine = "";
 
 gboolean global_engine_changed_cb(IBusBus *bus, gchar *name_c, gpointer user_data)
 {
     std::string name(name_c);
-    if (name == "kkc") {
+    if (name == japanese_engine) {
         std::cout << japanese_json << std::endl;
     } else {
         std::cout << std::endl;
@@ -15,8 +16,15 @@ gboolean global_engine_changed_cb(IBusBus *bus, gchar *name_c, gpointer user_dat
     return true;
 }
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        std::cerr << "No japanese engine provided" << std::endl;
+        return 1;
+    } else {
+        japanese_engine = std::string(argv[1]);
+    }
+
     ibus_init();
 
     IBusBus* bus = ibus_bus_new();
