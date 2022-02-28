47614614f8fea342f39ec99c94bdf7aa21a44aa3
Ho-Yon Mak <hoyon.mak@gmail.com>
2022-02-28 23:01:07 +0000

    sway: make media waybar widget more reliable

--- waybar/.config/waybar/mediaplayer.sh
+++ waybar/.config/waybar/mediaplayer.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-playerctl --player playerctld metadata --format '{{status}}	{{ artist }} - {{ title }}	{{playerName}}' --follow | jq --raw-input --unbuffered --compact-output 'split("\t") | {status: .[0], text: ((if (.[0] == "Paused") then " " else "" end) + .[1])}'
+cat <(playerctl status --follow & playerctl --player playerctld metadata --format '{{title}}' --follow) | while read -r line; do
+    players=$(playerctl --list-all 2>&1)
+    if [[ "$players" != "No players found" ]]; then
+        playerctl --player playerctld metadata --format '{{status}}	{{ artist }} - {{ title }}	{{playerName}}' |\
+            jq --raw-input --unbuffered --compact-output 'split("\t") | {status: .[0], text: ((if (.[0] == "Paused") then " " else "" end) + .[1])}'
+    else
+        echo ""
+    fi
+done
