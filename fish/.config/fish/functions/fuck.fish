3cd3140b3c6a98de69490931d83a9abd1a7e5187
Ho-Yon Mak <hoyon.mak@gmail.com>
2022-03-29 15:35:26 +0100

    fish: Check if thefuck installed

--- fish/.config/fish/functions/fuck.fish
+++ fish/.config/fish/functions/fuck.fish
@@ -1,5 +1,8 @@
-# Defined in /home/hoyon/.config/fish/config.fish @ line 102
 function fuck
-	thefuck --alias | source
-    fuck
+	if type -q thefuck
+      thefuck --alias | source
+      fuck
+  else
+      echo "thefuck not found"
+  end
 end
