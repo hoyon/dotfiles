7c8ad9ac26c70df50a2d13fd40e41164525608aa
Ho-Yon Mak <me@hoyon.dev>
2022-11-10 16:49:31 +0000

    fish: make aws-profile error on unknown profile

--- fish/.config/fish/functions/aws-profile.fish
+++ fish/.config/fish/functions/aws-profile.fish
@@ -2,6 +2,12 @@ function aws-profile --description="Set AWS_PROFILE env variable"
     if test (count $argv) -lt 1;
         set -ug AWS_PROFILE
     else
-        set -xg AWS_PROFILE $argv[1]
+        set profile "$argv[1]"
+        if grep -q "\[profile $profile\]" ~/.aws/config;
+            set -xg AWS_PROFILE $argv[1]
+        else
+            echo "Profile $profile not found in ~/.aws/config"
+            return 1
+        end
     end
 end
