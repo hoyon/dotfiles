fdd9663c471bda48ec130fe039aa41ab170a1358
Ho-Yon Mak <me@hoyon.dev>
2025-11-18 10:29:52 +0000

    fish: fix asdf config to avoid conflict with homebrew paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -85,9 +85,20 @@ if test -e ~/.asdf/asdf.fish
     source ~/.asdf/asdf.fish
 else if test -e /opt/asdf-vm/asdf.fish
     source /opt/asdf-vm/asdf.fish
-else if test -d /opt/homebrew/opt/asdf/libexec
-    source /opt/homebrew/opt/asdf/libexec/asdf.fish
 end
+
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.fish
     source /nix/var/nix/profiles/default/etc/profile.d/nix.fish
