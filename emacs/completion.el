4783c9219bab08dd63b7c53323747cfd67e56277
Ho-Yon Mak <me@hoyon.dev>
2025-12-19 15:27:37 +0000

    emacs: eglot config

--- emacs/completion.el
+++ emacs/completion.el
@@ -1,5 +1,11 @@
 ;; -*- lexical-binding: t -*-
 
 (use-package corfu
-  :config
   (global-corfu-mode))
+
+(setq eglot-code-action-indicator "!!"
+      eglot-code-action-indications '(margin))
+
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '(elixir-ts-mode . ("expert" "--stdio"))))
