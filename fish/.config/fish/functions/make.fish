a8b3ad53230793eaeed6c2bf9227f47107d0f74b
Ho-Yon Mak <me@hoyon.dev>
2026-04-13 14:24:44 +0100

    fish: make make.fish work on macos

--- fish/.config/fish/functions/make.fish
+++ fish/.config/fish/functions/make.fish
@@ -1,5 +1,11 @@
 # Defined in - @ line 1
 function make
-    set -l j_arg "-j"(nproc)
+    set -l ncpu
+    if command -q nproc
+        set ncpu (nproc)
+    else
+        set ncpu (sysctl -n hw.ncpu)
+    end
+    set -l j_arg "-j$ncpu"
     command make $j_arg $argv;
 end
